1e8f86d5f85aff56dba8a1c293f5aa56087a6244
mlarcode <maskew20@yahoo.com>
2016-08-01 07:50:49 -0400

    Add files via upload

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ///("|\_/|");
+            ///("/ @ @ \");
+            //("( > o < )");
+            //(" `>>X<<'  /"); 
+            //   (     ) /       
+            //    |   |
+
+
+            VirtualPet Animal = new VirtualPet();
+            string play;
+            do
+            {
+                Animal.Tick();
+                Animal.PrintStats();
+
+                Console.WriteLine("What do you want to do? Eat, exercise or shop");
+                string answer = Console.ReadLine();
+                if (answer == "Hunger")
+                {
+                    Animal.Eat();
+                }
+                else
+                {
+                    Animal.Play();
+                }
+
+                else
+                {
+                    Animal.Spend();
+                }
+
+                Animal.PrintStats();
+
+                Console.WriteLine("Do you want to go again?");
+                play = Console.ReadLine();
+
+            } while (play == "yes");
+            }
+    }
+    }
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        public class Animal
+        {
+            //Fields
+            private int hungry;
+            private int waste;
+            private int shop;
+            private int exercise;
+            private string nothing;
+
+            //Properties
+
+            public int Hunger 
+            {
+                get { return this.Hunger; }
+                set { this.Hunger = value; }
+            }
+
+            public int Waste
+            {
+                get { return this.waste; }
+                set { this.waste = value; }
+            }
+
+            public int Shop
+            {
+                get { return this.Shop; }
+                set { this.shop = value; }
+            }
+
+            public int Exercise
+            {
+                get { return this.exercise; }
+                set { this.exercise = value; }
+            }
+
+            public string DoNothing
+            {
+                get { return this.DoNothing; }
+                set { this.DoNothing = value; }
+            }
+
+            //Contructors
+            public Animal()
+            {
+                this.Hunger = 90;
+                this.waste = 50;
+                this.shop = 45;
+                this.exercise = 60;
+                this.DoNothing = ""; 
+            }
+
+            //Methods
+            public void Eat()
+            {
+                Hunger = Hunger + 40;
+            }
+            public void Play()
+            {
+                exercise = exercise + 120;
+            }
+            public void Spend()
+            {
+                shop = shop - 15;
+            }
+            public void PrintStats()
+            {
+                Console.WriteLine("Hunger" + this.Hunger);
+                Console.WriteLine("Exercise" + this.exercise);
+                Console.WriteLine("Shop" + this.shop);
+            }
+            public void Tick()
+            {
+                Hunger = Hunger + 70;
+                exercise = exercise + 120;
+                shop = shop - 20;
+
+            }
+            }
+        }
+    }
+
+
+
+
+
+
